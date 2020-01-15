6a93e7159f59a6070227acaa22eafa04ec5524e0
Shreyas <dlshreyas30@gmail.com>
2020-01-15 12:59:11 +0530

    Extra Section Added

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React,{ useContext} from 'react'
+import { Text, View, StyleSheet, Button, FlatList} from 'react-native'
+import BlogContext from '../context/BlogContext'
 
-const IndexScreen = () => {
+const IndexScreen = () =>{
+  const { data , addBlogPost} = useContext(BlogContext)
   return (
     <View>
-        <Text>Index Screen </Text>
+      <Text>Hello</Text>
+      <Button
+          title = 'Add BLog Post'
+          onPress =  { addBlogPost }
+      />
+      <FlatList
+          data = { data}
+          keyExtractor = { (blogPost) => blogPost.title}
+          renderItem = { ({ item }) => {
+            return <Text>{item.title}</Text>
+          }}
+      />
     </View>
-
   )
 }
 
